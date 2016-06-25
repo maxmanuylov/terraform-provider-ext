3565699f4eb0ef062dee4a4955e91ab6721dd94c
maxim.manuylov <maxim.manuylov@gmail.com>
2016-06-25 04:12:37 +0300

    version + typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1
+VERSION="v1.0-alpha.1"
 
 rm -rf bin
 
@@ -8,6 +8,6 @@ GOOS=darwin  GOARCH=amd64 go build -o bin/macos/terraform-provider-content
 GOOS=linux   GOARCH=amd64 go build -o bin/linux/terraform-provider-content
 GOOS=windows GOARCH=amd64 go build -o bin/windows/terraform-provider-content.exe
 
-tar czf bin/terraform-vsphere-$VERSION-macos.tar.gz --directory=bin/macos terraform-provider-content
-tar czf bin/terraform-vsphere-$VERSION-linux.tar.gz --directory=bin/linux terraform-provider-content
-zip     bin/terraform-vsphere-$VERSION-windows.zip -j bin/windows/terraform-provider-content.exe
+tar czf bin/terraform-provider-content-$VERSION-macos.tar.gz --directory=bin/macos terraform-provider-content
+tar czf bin/terraform-provider-content-$VERSION-linux.tar.gz --directory=bin/linux terraform-provider-content
+zip     bin/terraform-provider-content-$VERSION-windows.zip -j bin/windows/terraform-provider-content.exe
