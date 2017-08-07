d2d0f3f299b2a30d73f3b41f226a13a74212d9d8
maxim.manuylov <maxim.manuylov@gmail.com>
2017-08-07 18:56:57 +0300

    new intellij-hcl format

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ rm -rf vendor
 govendor init
 
 govendor fetch github.com/hashicorp/terraform@=$TERRAFORM_VERSION
-govendor fetch github.com/maxmanuylov/utils/intellij-hcl/terraform/provider-schema-generator@=terraform-$TERRAFORM_VERSION
+govendor fetch github.com/maxmanuylov/utils/intellij-hcl/terraform/provider-schema-generator@=v2
