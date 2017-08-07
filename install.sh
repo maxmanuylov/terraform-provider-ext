285503d213c427b38d0e1705ae34cf7f1cb654f3
maxim.manuylov <maxim.manuylov@gmail.com>
2017-08-07 18:58:49 +0300

    minor

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-INTELLIJ_PLUGINS_DIR="<Insert your value>"
 TERRAFORM_DIR="<Insert your value>"
 LOCAL_OS="<Insert your value>"
 
-cp -f bin/ext.json "$INTELLIJ_PLUGINS_DIR/intellij-hcl/classes/terraform/model/providers/ext.json"
+cp -f bin/ext.json "$HOME/.terraform.d/schemas/ext.json"
 cp -f "bin/$LOCAL_OS/terraform-provider-ext" "$TERRAFORM_DIR/terraform-provider-ext"
